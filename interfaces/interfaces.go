66cd966d50c0718e4058e441ed0025aeabd630bb
diamonddelt <rrrasti@yahoo.com>
2017-07-03 19:22:02 -0500

    Finish up interface examples

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 // Examine how interfaces work in conjunction with structs and methods
 // The examples include electronic machinery and vehicles
@@ -64,4 +67,27 @@ func main() {
 	for _, value := range x {
 		value.GenerateElectricity()
 	}
+
+	// examine how empty interfaces implement 0 methods, so any 'type' can also be an interface
+	var blankInterface interface{}
+	i := 14                                     // a random int
+	s := "just a lonely string"                 // a random string
+	v := Vehicle{Machine{300}, 85, 21, "Mazda"} // a random Vehicle
+	blankInterface = i                          // this is valid
+	blankInterface = s                          // so is this
+	blankInterface = v                          // this too
+	fmt.Println(blankInterface)                 // lets see what the interface is now
+
+	// examine how to 'assert' that a certain type is stored in an interface variable (since it can potentially be anything)
+	// 1. Assertion-ok pattern
+	isAVehicle, ok := blankInterface.(Vehicle) // ok will be true if the interface variable contains the type of interface after the dot-assertion
+	if ok {
+		fmt.Printf("The assertion passed i.e. blankInterface is a Vehicle with the following attributes: %v\n", isAVehicle)
+	} else {
+		fmt.Printf("The assertion failed i.e. blankInterface is not currently an interface of type Vehicle.\n")
+	}
+
+	// 2. Reflection package (not necessary for all use cases)
+	var reflectMachine interface{} = Machine{35}
+	fmt.Println(reflect.TypeOf(reflectMachine))
 }
