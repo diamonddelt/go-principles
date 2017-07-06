b617e5b78a0861e91ed0bd9d182f230eb7c4a0bf
diamonddelt <rrrasti@yahoo.com>
2017-07-05 19:38:03 -0500

    Complete 3 examples of basic Go concurrency

--- concurrency/range-over-channels.go
+++ concurrency/range-over-channels.go
@@ -3,21 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	c := make(chan int, 5)  // create an int channel with a buffered capacity of 3 for use with factorial()
-	go factorial(cap(c), c) // spawn a goroutine to run factorial(); cap() specifies the capacity of the channel
+	c := make(chan int, 5) // create an int channel with a buffered capacity of 3 for use with factorial()
+	go counter(cap(c), c)  // spawn a goroutine to run factorial(); cap() specifies the capacity of the channel
 
 	count := 1
-	for i := range c {
+	for i := range c { // this line is important - without the explicit close(c) in counter(), this range over a channel would deadlock
 		fmt.Println("The current factorial value at iteration", count, "is:", i)
 		count++
 	}
 }
 
-func factorial(n int, c chan int) {
-	f := 1
-	for i := 1; i < (n + 1); i++ {
-		c <- f // send the current factorial value to the channel
-		f *= i // get new factorial value
+func counter(n int, c chan int) {
+	count := 1
+	for i := 0; i < n; i++ {
+		c <- count // send the current count to the channel
+		count++
 	}
 	close(c) // explicitly close the channel; this is needed for goroutine access-safety in the caller
 	// "close should always be executed by the sender, never the receiver" <- https://golang.org/pkg/builtin/#close
